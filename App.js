74b80e70820e73685e37c15b772e877daacd605d
Lizzy Harvey <lizzy220188@gmail.com>
2022-09-04 14:09:04 -0400

    Added PreparingOrderScreen

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
 import BasketScreen from './screens/BasketScreen';
+import PreparingOrderScreen from './screens/PreparingOrderScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -23,6 +24,8 @@ export default function App() {
             <Stack.Screen name="Restaurant" component={RestaurantScreen} />
             <Stack.Screen name="Basket" component={BasketScreen} 
             options={{presentation: 'modal', headerShown: false}}/>
+            <Stack.Screen name="PreparingOrderScreen" component={PreparingOrderScreen}
+            options={{presentation: 'fullScreenModal', headerShown: false}}/>
           </Stack.Navigator>
         </TailwindProvider>
       </Provider>
